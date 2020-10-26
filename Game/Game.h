1844565e7ccc1d79c111f5f7b160529bb5e5e62c
PENPITCHA SAPPAKUN <63010709@KMITL.AC.TH>
2020-10-26 07:56:36 +0700

    1

--- Game/Game.h
+++ Game/Game.h
@@ -1,14 +1,16 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+
 #include"Player.h"
 #include"Knife.h"
 #include"Sword.h"
+
 #include"Machine.h"
 #include"Machineanimation.h"
+
 #include"Treeanimation.h"
 #include"Treeanimation2.h"
 #include"Treeanimation3.h"
-
 
 class Game
 {
---
+++ Game/MainMenu.h
@@ -0,0 +1,10 @@
+#pragma once
+class MainMenu
+{
+private:
+	void initKeybinds();
+
+public:
+	MainMenu (sf::RenderWindow*window,)
+};
+
